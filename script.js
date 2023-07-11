3265833aefebfef7dffb3cbd45682efff9db3799
Karam Singh <karam.singh7@gmail.com>
2023-07-11 22:51:12 +0530

    Update

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 const toggleMenu = () => document.body.classList.toggle("open");
 
+// Get the sections and the previous/next buttons
 const sections = Array.from(document.getElementsByClassName('head'));
 const site = document.getElementById('site');
+const previousButtons = Array.from(document.getElementsByClassName('pre'));
+const nextButtons = Array.from(document.getElementsByClassName('next'));
 
-let currentIndex = 0;
+let currentIndex = 0; // Current section index
 
+// Function to show the current section and hide the rest
 function showSection(index) {
     sections.forEach((section, i) => {
         section.style.display = i === index ? 'grid' : 'none';
     });
 }
 
-
+// Function to handle the next button click
 function handleNextClick() {
     currentIndex = (currentIndex + 1) % sections.length;
     showSection(currentIndex);
 }
 
+// Function to handle the previous button click
 function handlePreviousClick() {
     currentIndex = (currentIndex - 1 + sections.length) % sections.length;
     showSection(currentIndex);
 }
 
-site.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.id === 'next') {
-        handleNextClick();
-    } else if (target.id === 'pre') {
-        handlePreviousClick();
-    }
+// Attach event listeners to previous buttons
+previousButtons.forEach((button) => {
+    button.addEventListener('click', handlePreviousClick);
 });
 
+// Attach event listeners to next buttons
+nextButtons.forEach((button) => {
+    button.addEventListener('click', handleNextClick);
+});
+
+// Show the initial section
 showSection(currentIndex);
